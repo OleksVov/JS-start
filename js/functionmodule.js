02bdbf1d566f4fbb863f02b7c19172d72c0296c1
Oleksandr <alex.vovchok@gmail.com>
2022-12-24 19:50:34 +0200

    Update functionmodule.js

--- js/functionmodule.js
+++ js/functionmodule.js
@@ -29,3 +29,19 @@
 //     }
     
 // console.log(min(14, 11));
+
+const book = {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    genres: ["historical prose", "adventure"],
+    isPublic: true,
+    rating: 8.38,
+  };
+  
+book.pageCount = 836;
+book.originalLanguage = "en";
+book.translations = ["ua", "ru"];
+
+console.log(book); // 836
+console.log(book.originalLanguage); // 'en'
+console.log(book.translations); // ['ua', 'ru']
