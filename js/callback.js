654f6f1a3204a58b07137a71d5653bba4b7c1b8d
Oleksandr <alex.vovchok@gmail.com>
2023-01-03 11:51:17 +0200

    Update callback.js

--- js/callback.js
+++ js/callback.js
@@ -25,18 +25,30 @@ function processCall(recipient, onAvailable, onNotAvailable) {
     // Логика записи голограммы
   }
   
-  processCall("Манго", takeCall, activateAnsweringMachine);
-  processCall("Поли", takeCall, leaveHoloMessage);
+  // processCall("Манго", takeCall, activateAnsweringMachine);
+  // processCall("Поли", takeCall, leaveHoloMessage);
 
+  // forEach
 
-  const numbers = [5, 10, 15, 20, 25];
+//   const numbers = [5, 10, 15, 20, 25];
 
-// Классический for
-for (let i = 0; i < numbers.length; i += 1) {
-  console.log(`Индекс ${i}, значение ${numbers[i]}`);
-}
+// // Классический for
+// for (let i = 0; i < numbers.length; i += 1) {
+//   console.log(`Индекс ${i}, значение ${numbers[i]}`);
+// }
 
-// Перебирающий forEach
-numbers.forEach(function (number, index) {
-  console.log(`Индекс ${index}, значение ${number}`);
-});
+// // Перебирающий forEach
+// numbers.forEach(function (number, index) {
+//   console.log(`Индекс ${index}, значение ${number}`);
+// });
+
+const planets = ["Земля", "Марс", "Венера", "Юпитер"];
+
+const planetsInUpperCase = planets.map(planet => planet.toUpperCase());
+console.log(planetsInUpperCase); // ['ЗЕМЛЯ', 'МАРС', 'ВЕНЕРА', 'ЮПИТЕР']
+
+const planetsInLowerCase = planets.map(planet => planet.toLowerCase());
+console.log(planetsInLowerCase); // ['земля', 'марс', 'венера', 'юпитер']
+
+// Оригинальный массив не изменился
+console.log(planets); // ['Земля', 'Марс', 'Венера', 'Юпитер']
